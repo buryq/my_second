e2ae8a34f17ed8c18adb4eeaceefee89fb74908f
buryq <s_high@163.com>
2020-01-30 13:33:54 +0800

    git commit -a

--- 1.js
+++ 1.js
@@ -1,3 +1,31 @@
-console.log('This is test');
+function countBits(n) {
 
-console.log('Do not download.');
+    let result = 0;
+
+    //do sth here
+    let b = (n).toString(2);
+
+    // console.log('b is ...', b);
+
+    // b.forEach(item => {
+    //     if (item === '1') {
+    //         result++;
+    //     }
+    // })
+
+    // console.log('b is ', typeof (b));
+
+    console.log(`binary of ${n} is ${b}`);
+
+    for (let i = 0; i < b.length; i++) {
+        if (b[i] === '1') {
+            result++;
+        }
+    }
+
+
+    return result;
+}
+
+let o = countBits(9651254);
+console.log(`It has ${o} of 1`);
